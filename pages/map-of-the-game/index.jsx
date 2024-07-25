7707da1e557cea63d58b30be5f886f2f214f0b61
StefanV <stefan@veelancing.io>
2024-07-25 22:07:07 +0300

    commit

--- pages/map-of-the-game/index.jsx
+++ pages/map-of-the-game/index.jsx
@@ -27,11 +27,26 @@ export default function MapOfTheGame() {
     const [showMap, setShowMap] = useState(false);
     const [showMapInput, setShowMapInput] = useState('');
     const [showWinnieLoading, setShowWinnieLoading] = useState(true);
+    const [showPuzzle, setShowPuzzle] = useState(false);
+    const [puzzleSolved5, setPuzzleSolved5] = useState(false);
+    const [showInitialGif, setShowInitialGif] = useState(true);
     const correctMapInput = 'head-over-heels-for-you';
+
+    React.useEffect(() => {
+      setTimeout(() => {
+        setShowInitialGif(false);
+      }, 7000);
+    }, []);
+    
+    React.useEffect(() => {
+      const puzzleSolved5 = localStorage.getItem('puzzleSolved5');
+      if (puzzleSolved5) {
+        setPuzzleSolved5(true);
+      }
+    }, []);
 
     const handleChangeMapInput = (e) => {
         setShowMapInput(e.target.value);
-        // localStorage.setItem('showMapInput', e.target.value);
         
     }
 
@@ -46,38 +61,6 @@ export default function MapOfTheGame() {
         
     }
 
-  const [clockValue, setClockValue] = useState('2024-05-10T09:25:05.451Z');
-  const [clockDisplayed, setClockDisplayed] = useState(false);
-  const [clockInput, setClockInput] = useState('');
-  const correctClockInput = 'clock';
-  const [showPuzzle, setShowPuzzle] = useState(false);
-  const [puzzleSolved5, setPuzzleSolved5] = useState(false);
-  const [showSecondPuzzle, setShowSecondPuzzle] = useState(false);
-  const [puzzleInput, setPuzzleInput] = useState('');
-  const correctPuzzleInput = 'puzzle';
-
-  // next clue -> home-town-childhood-parc
-
-
-const handleChangePuzzleInput = (e) => {
-  setPuzzleInput(e.target.value);
-  // localStorage.setItem('showMapInput', e.target.value);
-}
-
-const showPuzzleFunc = () => {
-
-    if (puzzleInput !== correctPuzzleInput) {
-        toast.error("Incorrect Puzzle Input", {
-            icon: "⏃",
-        });
-        return;
-    }
-    toast.success("Puzzle Solved", {
-        icon: "🧩"
-    }),
-    setShowSecondPuzzle(true);
-}
-
 const puzzleSolvedFunc = () => {
     setPuzzleSolved5(true);
     localStorage.setItem('puzzleSolved5', true);
@@ -89,131 +72,132 @@ const puzzleSolvedFunc = () => {
     }, 3000);
 }
 
-React.useEffect(() => {
-  const puzzleSolved5 = localStorage.getItem('puzzleSolved5');
-  if (puzzleSolved5) {
-    setPuzzleSolved5(true);
-  }
-}, []);
 
-const handleChangeClockInput = (e) => {
-  setClockInput(e.target.value);
-  // localStorage.setItem('showMapInput', e.target.value);
-}
 
-const showClockFunc = () => {
-    if (clockInput !== correctClockInput) {
-        toast.error("Incorrect Clock Input", {
-            icon: "⏃",
-        });
-        return;
-    }
-    setClockDisplayed(true);
-}
 
     return  (
         <>
             <Container maxWidth="xxl" className="map-main-container">
-             {!showMap ? (
+             {!showInitialGif ? (
               <>
-              <Box style={{ marginBottom: '2rem' }}>
-               {showPuzzle ? (
-                <>
-                  <JigsawPuzzle
-                    imageSrc="images/puzzle4.jpg"
-                    rows={2}
-                    columns={3}
-                    onSolved={() => puzzleSolvedFunc()}
-                  />
-                </>
-               ) : (
-                <>
-                  {!puzzleSolved5 ? (
+               <div className="main-content">
+                {!showMap ? (
+                  <>
+                  <Box style={{ marginBottom: '2rem' }}>
+                  {showPuzzle ? (
                     <>
+                      <JigsawPuzzle
+                        imageSrc="images/puzzle4.jpg"
+                        rows={2}
+                        columns={3}
+                        onSolved={() => puzzleSolvedFunc()}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      {!puzzleSolved5 ? (
+                        <>
+                          <Box style={{ marginTop: '1rem' }}>
+                            <Button
+                              className="map-submit-btn2"
+                              variant="contained"
+                              onClick={() => setShowPuzzle(true)}
+                            >
+                              Begin Puzzle Game ❤️
+                            </Button>
+                          </Box>
+                        </>
+                      ) : (
+                        <>
+                          <Typography className="map-temp-text1" variant="h2">
+                            Use the riddle from the last envelope to unlock next location ❤️
+                          </Typography>
+                          <br /><br />
+                          <Typography className="map-temp-text2" variant="h2">
+                            HINT: <br />
+                            <br />
+                            The Jack of Hearts is a court card that is deeply associated with love.<br />
+                            Mirroring the Knight of Cups in tarot,
+                            which represents a knight in shining armor. <br />
+                            <br />
+                            The Jack of Hearts connotes
+                            a young man who is head over heels for someone. <br />
+                            The card has an innocence to it, suggesting puppy love.
+                          </Typography>
+                        </>
+                      )}
+                    </>
+                  )}
+                </Box>
+                  {puzzleSolved5 ? (
+                    <>
+                      <Form style={{ marginTop: '2rem' }}>
+                        <input
+                          type="text"
+                          className="show-map-btn-input"
+                          fluid
+                          placeholder='Write Your Answer'
+                          name="showMapInput"
+                          id="showMapInput"
+                          value={showMapInput}
+                          onChange={handleChangeMapInput}
+                        />
+                      </Form>
                       <Box style={{ marginTop: '1rem' }}>
                         <Button
                           className="map-submit-btn2"
                           variant="contained"
-                          onClick={() => setShowPuzzle(true)}
+                          onClick={showMapFunc}
                         >
-                          Begin Puzzle Game ❤️
+                          Submit
                         </Button>
                       </Box>
                     </>
                   ) : (
+                    <></>
+                  )}
+                  </>
+                ) : (
+                  <>
+                  </>
+                )}
+                  {!showMap && showWinnieLoading ? (
                     <>
-                      <Typography className="map-temp-text1" variant="h2">
-                        Use the riddle from the last envelope to unlock next location ❤️
-                      </Typography>
-                      <br /><br />
-                      <Typography className="map-temp-text2" variant="h2">
-                        HINT: <br />
-                        <br />
-                        The Jack of Hearts is a court card that is deeply associated with love.<br />
-                        Mirroring the Knight of Cups in tarot,
-                        which represents a knight in shining armor. <br />
-                        <br />
-                        The Jack of Hearts connotes
-                        a young man who is head over heels for someone. <br />
-                        The card has an innocence to it, suggesting puppy love.
-                      </Typography>
+                      <div style={{ marginTop: '2rem' }}>
+                        <img className="gif-style" src="images/dalmatians.gif" />
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div style={{ marginTop: '5rem' }}>
+                    <     Typography style={{ marginBottom: '2rem' }} className="map-temp-text1" variant="h2">
+                            Bellow you will find our next rendezvous place ❤️
+                          </Typography>
+                        <ImageZoom
+                          src="images/harta10.jpg"
+                          zoom={300}
+                          style={{ marginLeft: '-2rem' }}
+                        />
+                        </div>
                     </>
                   )}
-                </>
-               )}
-             </Box>
-              {puzzleSolved5 ? (
-                <>
-                  <Form style={{ marginTop: '2rem' }}>
-                    <input
-                      type="text"
-                      className="show-map-btn-input"
-                      fluid
-                      placeholder='Write Your Answer'
-                      name="showMapInput"
-                      id="showMapInput"
-                      value={showMapInput}
-                      onChange={handleChangeMapInput}
-                    />
-                  </Form>
-                  <Box style={{ marginTop: '1rem' }}>
-                    <Button
-                      className="map-submit-btn2"
-                      variant="contained"
-                      onClick={showMapFunc}
-                    >
-                      Submit
-                    </Button>
-                  </Box>
-                </>
-              ) : (
-                <></>
-              )}
+               </div>
               </>
              ) : (
               <>
+                 <div className="initial-gif-container">
+                  <Stack direction="row" spacing={2}>
+                    <Typography variant="h4" className="loading-text1">
+                      Loading Princess
+                    </Typography>
+                    <img className="gif-loading-style" src="images/bird.gif" alt="Loading GIF" />
+                  </Stack>
+                    <Box style={{ display: 'flex', justifyContent: 'center' }}>
+                        <img className="gif-style" src="images/sleeping.gif" alt="Initial GIF" />
+                    </Box>
+                 </div>
               </>
              )}
-              {!showMap && showWinnieLoading ? (
-                <>
-                  <div style={{ marginTop: '2rem' }}>
-                    <img className="gif-style" src="images/dalmatians.gif" />
-                  </div>
-                </>
-              ) : (
-                 <>
-                  <div style={{ marginTop: '5rem' }}>
-                <     Typography style={{ marginBottom: '2rem' }} className="map-temp-text1" variant="h2">
-                        Bellow you will find our next rendezvous place ❤️
-                      </Typography>
-                    <ImageZoom
-                      src="images/harta10.jpg"
-                      zoom={300}
-                      style={{ marginLeft: '-2rem' }}
-                    />
-                    </div>
-                 </>
-              )}
               <ParticleImage
                 style={{ marginLeft: '-1rem' }}
                 scale={0.55}
